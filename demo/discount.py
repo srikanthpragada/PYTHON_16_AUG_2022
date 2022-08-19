1829b2435360eea3c489093965d70d8fec6ca587
srikanthpragada <srikanthpragada@yahoo.com>
2022-08-19 07:18:27 +0530

    19-AUG

---
+++ demo/discount.py
@@ -0,0 +1,6 @@
+# Take price and display discount @ 15%
+price = float(input("Enter price :"))  # Convert str to float
+discount = price * 0.15
+print(discount)
+
+
---
+++ demo/hello.py
@@ -0,0 +1,2 @@
+print("Hello Python")
+print("I am learning Python")
